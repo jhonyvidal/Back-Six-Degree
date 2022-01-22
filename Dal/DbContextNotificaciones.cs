afbe9a23f64bdd0e06ebb0537afb65f43dbcb7e2
jhony german vidal lopez <jhonylopez147@hotmail.com>
2022-01-22 10:12:22 -0500

    Primer Cambio

---
+++ Dal/DbContextNotificaciones.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Datos
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+
+    class RHcontext
+    {
+        public GestionHumanaContext CrearContext()
+        {
+            var options = new DbContextOptionsBuilder<GestionHumanaContext>()
+            .UseSqlServer(@"data source=192.168.93.193,1433\\SQLEXPRESS;initial catalog=PruebaSD;user id=sa;password=JXPr3ns42021;persist security info=True;")
+            .Options;
+            return  new GestionHumanaContext(options);
+        }
+
+    }
+    public partial class GestionHumanaContext : DbContext
+    {
+        //public GestionHumanaContext(DbContextOptions<GestionHumanaContext> options) : base(options)
+        //    {
+        //    }
+        public GestionHumanaContext(DbContextOptions options) : base(options)
+        {
+        }
+        //INICIO MAPEO
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.ApplyConfiguration(new usuarioMap());
+
+
+        }
+        //FIN MAPEO
+
+        internal static object createDbContext()
+        {
+            throw new NotImplementedException();
+        }
+        
+        //INICIO ENTIDADES
+        public virtual DbSet<Usuario> Usuario { get; set; }
+
+        //FIN ENTIDADES
+    }
+}
---
+++ Web/Controllers/UsuarioController.cs
@@ -0,0 +1,81 @@
+﻿using Beans;
+using BLL;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+
+    [Route("api/[controller]")]
+        [ApiController]
+        public class UsuarioController : ControllerBase
+        {
+      
+        UsuarioBll UsuarioBll = new UsuarioBll();
+
+        // GET: api/Usuario/List/0
+        [HttpGet("[action]/{id}")]
+        public  Task<IEnumerable<UsuarioBean>> List([FromRoute] int id)
+        {
+
+            return UsuarioBll.ListUsuario(id);
+        }
+        // GET: api/Usuario/ListText/texto
+        [HttpGet("[action]/{Texto}")]
+        public Task<IEnumerable<UsuarioBean>> ListText([FromRoute] string Texto)
+        {
+            return UsuarioBll.ListUsuarioTexto(Texto);
+        }
+
+
+        // POST: api/Usuario/Insert
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Insert([FromBody] UsuarioBean model)
+        {
+            if (!ModelState.IsValid) { return BadRequest(ModelState); }
+
+            //Insert Datos y Envio de correo
+            var consulta = await UsuarioBll.InsertUsuario(model);
+            if(consulta == false) { return BadRequest(); }
+
+            return Ok();
+
+        }
+
+        // PUT: api/Usuario/Update
+        [HttpPut("[action]")]
+        public async Task<IActionResult> Update([FromBody] UsuarioBean model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (model.id <= 0)
+            {
+                return BadRequest();
+            }
+            var consulta = await UsuarioBll.UpdateUsuario(model);
+
+            if (consulta == true) { return Ok(); }
+            else { return BadRequest(); }
+        }
+        // DELETE: api/Libro/Delete/5
+        [HttpDelete("[action]/{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var consulta = await UsuarioBll.DeleteUsuario( id);
+
+            if (consulta == "ok") { return Ok(); }
+            else { return BadRequest(consulta); }
+        }
+
+    }
+}
+
